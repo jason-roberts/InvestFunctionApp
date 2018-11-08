0ec8f30740124f5155b39ff4732f20880aa4d82f
Jason Roberts <osjasonroberts@gmail.com>
2018-11-08 12:12:56 +0800

    Update src/InvestFunctionApp/InvestFunctionApp/Portfolio.cs

--- src/InvestFunctionApp/InvestFunctionApp/Portfolio.cs
+++ src/InvestFunctionApp/InvestFunctionApp/Portfolio.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
